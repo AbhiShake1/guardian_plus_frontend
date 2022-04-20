48f0c4f2778087b30460f945796a53965690e7d4
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-20 10:56:10 +0545

    fix wrong

--- lib/api/rest_client.g.dart
+++ lib/api/rest_client.g.dart
@@ -1,16 +1,14 @@
 part of 'rest_client.dart';
 
 class RestClientImpl implements RestClient {
-  final _headers = {'Access-Control-Allow-Origin': "*"};
-
   @override
   Future<UserModel?> login(
       {required String userId, required String password}) async {
     final _data = {'uid': userId, 'password': password};
     final _result = await post(
-        Uri.parse('https://ams-production-ae56.up.railway.app/api=auth/signin/'),
-        body: jsonEncode(_data),
-        headers: _headers);
+      Uri.parse('https://guardianplus-production.up.railway.app/api=auth/signin/'),
+      body: jsonEncode(_data),
+    );
     final value = _result.statusCode >= 400
         ? null
         : UserModel.fromJson(jsonDecode(_result.body));
@@ -20,9 +18,9 @@ class RestClientImpl implements RestClient {
   @override
   Future<UserModel?> currentUser() async {
     final _result = await get(
-        Uri.parse(
-            'https://ams-production-ae56.up.railway.app/api=auth/current_user/'),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=auth/current_user/'),
+    );
     final value = _result.statusCode >= 400
         ? null
         : UserModel.fromJson(jsonDecode(_result.body));
@@ -32,8 +30,8 @@ class RestClientImpl implements RestClient {
   @override
   Future<String?> logout() async {
     final _result = await get(
-        Uri.parse('https://ams-production-ae56.up.railway.app/api=auth/signout/'),
-        headers: _headers);
+      Uri.parse('https://guardianplus-production.up.railway.app/api=auth/signout/'),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
@@ -42,10 +40,10 @@ class RestClientImpl implements RestClient {
   Future<String?> updatePassword({required password}) async {
     final _data = {'password': password};
     final _result = await put(
-        Uri.parse(
-            'https://ams-production-ae56.up.railway.app/api=auth/update_password/'),
-        body: jsonEncode(_data),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=auth/update_password/'),
+      body: jsonEncode(_data),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
@@ -53,9 +51,9 @@ class RestClientImpl implements RestClient {
   @override
   Future<String?> getChildren() async {
     final _result = await get(
-        Uri.parse(
-            'https://ams-production-ae56.up.railway.app/api=parent/get_children/'),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=parent/get_children/'),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
@@ -64,10 +62,10 @@ class RestClientImpl implements RestClient {
   Future<String?> getChildSubjects({required userId}) async {
     final _data = {'uid': userId};
     final _result = await post(
-        Uri.parse(
-            'https://ams-production-ae56.up.railway.app/api=parent/get_child_subjects/'),
-        body: jsonEncode(_data),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=parent/get_child_subjects/'),
+      body: jsonEncode(_data),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
@@ -76,10 +74,10 @@ class RestClientImpl implements RestClient {
   Future<String?> getChildProgress({required userId}) async {
     final _data = {'uid': userId};
     final _result = await post(
-        Uri.parse(
-            'https://ams-production-ae56.up.railway.app/api=child/get_child_progress/'),
-        body: jsonEncode(_data),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=child/get_child_progress/'),
+      body: jsonEncode(_data),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
@@ -87,9 +85,9 @@ class RestClientImpl implements RestClient {
   @override
   Future<List<AssessmentModel?>?> getAssessments() async {
     final _result = await get(
-        Uri.parse(
-            'https://ams-production-ae56.up.railway.app/api=assessments/get_all/'),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=assessments/get_all/'),
+    );
     final List<AssessmentModel?> value = [];
     (jsonDecode(_result.statusCode >= 400 ? '' : _result.body) ?? '').forEach(
         (i) => value.add(AssessmentModel.fromJson(jsonDecode(jsonEncode(i)))));
@@ -99,8 +97,9 @@ class RestClientImpl implements RestClient {
   @override
   Future<List<NoticeModel?>?> getNotices() async {
     final _result = await get(
-        Uri.parse('https://ams-production-ae56.up.railway.app/api=notice/get_all/'),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=notice/get_all/'),
+    );
     final List<NoticeModel?> value = [];
     (jsonDecode(_result.statusCode >= 400 ? '' : _result.body) ?? '')
         .forEach((i) => value.add(NoticeModel.fromJson(jsonDecode(jsonEncode(i)))));
@@ -111,9 +110,9 @@ class RestClientImpl implements RestClient {
   Future<String?> postFeedback(String title, String description) async {
     final _data = {'title': title, 'description': description};
     final _result = await post(
-        Uri.parse('https://ams-production-ae56.up.railway.app/api=feedback/post/'),
-        body: jsonEncode(_data),
-        headers: _headers);
+      Uri.parse('https://guardianplus-production.up.railway.app/api=feedback/post/'),
+      body: jsonEncode(_data),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
@@ -122,9 +121,10 @@ class RestClientImpl implements RestClient {
   Future<String?> getRoutine(String uid) async {
     final _data = {'uid': uid};
     final _result = await post(
-        Uri.parse('https://ams-production-ae56.up.railway.app/api=routine/get_all/'),
-        body: jsonEncode(_data),
-        headers: _headers);
+      Uri.parse(
+          'https://guardianplus-production.up.railway.app/api=routine/get_all/'),
+      body: jsonEncode(_data),
+    );
     final value = _result.statusCode >= 400 ? null : _result.body;
     return value;
   }
