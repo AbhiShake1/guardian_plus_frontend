a31865bcef6fec2451d797b52b7e620d5bb1e0fa
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-25 08:04:49 +0545

    Update assignments.dart

--- lib/feature/assignments/assignments.dart
+++ lib/feature/assignments/assignments.dart
@@ -45,7 +45,7 @@ class Assignments extends ConsumerWidget {
                     margin: const EdgeInsets.all(5.0),
                     child: ListTile(
                       title: Text(
-                        assessments[index]?.task ?? '',
+                        assessments[index]?.subject ?? '',
                         style: const TextStyle(fontSize: 20.0, color: Colors.white),
                       ),
                       onTap: () {
